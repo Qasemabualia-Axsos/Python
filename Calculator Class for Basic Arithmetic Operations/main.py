3ea23e12a05c9b82a5bc13abf5f432bb2a5f4de6
Qassim Ehab kamel Aboalia <qassimalia77@gmail.com>
2025-07-22 18:31:58 +0300

    Add files via upload

---
+++ Calculator Class for Basic Arithmetic Operations/main.py
@@ -0,0 +1,51 @@
+class Calculator:
+    def __init__(self,a=0,b=0):
+        self.a=a
+        self.b=b
+    def error_handle(self):
+        if not isinstance(self.a,int) or not isinstance(self.b,int):
+            self.result="Error:the input must be Number"
+            return False
+        return True
+    def add(self):
+        if self.error_handle():
+            self.result=self.a+self.b
+        return self
+    def substract(self):
+        if self.error_handle():
+            self.result=self.a-self.b
+        return self
+    def multiply(self):
+        if self.error_handle():
+            self.result=self.a*self.b
+        return self
+    def divide (self):
+        if not self.error_handle():
+            return self
+        elif self.b==0:
+            self.result= "Error:Divesion by zero"
+        else:
+            self.result=self.a/self.b
+        return self
+    def show_result(self):
+        print(f"Result:{self.result}")
+        return self
+    
+cal=Calculator(10,5)
+cal.add().show_result()
+cal.substract().show_result()
+cal.multiply().show_result()
+cal.divide().show_result()
+print("---------------")
+cal=Calculator(100,0)
+cal.add().show_result()
+cal.substract().show_result()
+cal.multiply().show_result()
+cal.divide().show_result()
+print("---------------")
+cal=Calculator("Qasem",20)
+cal.add().show_result()
+cal.substract().show_result()
+cal.multiply().show_result()
+cal.divide().show_result()
+

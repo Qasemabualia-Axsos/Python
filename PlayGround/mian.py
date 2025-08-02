5c5b7111bb48b5d227623542e0e69aa9e907d5ee
Qasem Ehab kamel Aboalia <qassimalia77@gmail.com>
2025-08-02 11:40:11 +0300

    Update mian.py

--- PlayGround/mian.py
+++ PlayGround/mian.py
@@ -3,15 +3,15 @@ from flask import Flask,render_template
 app=Flask(__name__)
 
 @app.route("/play")
-def first():
+def three_boxes():
     return render_template("index.html",num=3,color="#9fc5f8")
 
 @app.route("/play/<x>")
-def second(x):
+def custom_boxes(x):
     return render_template ("index.html",num=int(x),color="#9fc5f8")
 
 @app.route("/play/<x>/<color>")
-def third(x,color):
+def custom_color(x,color):
     return render_template ("index.html",num=int(x),color=color)
 
 if __name__==("__main__"):

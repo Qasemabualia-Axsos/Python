b32dc71c33d34e0cbdd9b01d59a5d6d51ee755d5
Qasem Ehab kamel Aboalia <qassimalia77@gmail.com>
2025-08-02 11:41:42 +0300

    Update main.py

--- Checkerboard/main.py
+++ Checkerboard/main.py
@@ -4,15 +4,18 @@ app=Flask(__name__)
 
 @app.route ("/")
 def eight_eight():
-    return render_template ('index.html',rows=8,cols=8)
+    return render_template ('index.html',rows=8,cols=8,color1="black",color2="red")
 
 @app.route ("/<int:cols>")
 def eight_four(cols):
-    return render_template ('index.html',rows=8,cols=cols)
+    return render_template ('index.html',rows=8,cols=cols,color1="black",color2="red")
 
 @app.route("/<int:rows>/<int:cols>")
 def custom_board(rows,cols):
-    return render_template ('index.html',rows=rows,cols=cols)
-
+    return render_template ('index.html',rows=rows,cols=cols,color1="black",color2="red")
+    
+@app.route("/<int:rows>/<int:cols>/<color1>/<color2>")
+def alternating_colors(rows,cols,color1,color2):
+    return render_template('index.html',rows=rows,cols=cols,color1=color1,color2=color2)
 if __name__=="__main__":
     app.run(debug=True,port=7000)

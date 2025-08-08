60e1486b9209578a5348a6f9af7ed1501eba47d5
Qasem Ehab kamel Aboalia <qassimalia77@gmail.com>
2025-08-08 10:25:26 +0300

    Update main.py

--- MathDojo/main.py
+++ MathDojo/main.py
@@ -1,26 +1,29 @@
 class MathDojo:
     def __init__(self):
         self.result=0
+        
     def add(self,num,*nums):
         self.result+=num
         for n in nums:
             self.result+=n
         return self
     
-    def sustract(self,num,*nums):
+    def subtract(self,num,*nums):
         self.result-=num
         for n in nums:
             self.result-=n
         return self
 
-md=MathDojo()
 
-x=md.add(5).add(10,2).add(2,4,6).result
-print(x)
+md = MathDojo()
+x = md.add(5).add(10, 2).add(2, 4, 6).result
+print(x) 
 
 
-x=md.add(2).add(2,5,1).sustract(3,2).result
-print(x)
+md = MathDojo()
+x = md.add(2).add(2, 5, 1).subtract(3, 2).result
+print(x) 
 
-x=md.sustract(5).sustract(10,6).sustract(5,4,8).result
+md = MathDojo()
+x = md.subtract(5).subtract(10, 6).subtract(5, 4, 8).result
 print(x)
